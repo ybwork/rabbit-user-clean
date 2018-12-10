a1e634cb8cc5b165946e2bb1220d475e8375afda
Ilya Kaduk <kaduk2593@gmail.com>
2018-12-10 17:06:52 +0300

    Fix code main code style

--- task.py
+++ task.py
@@ -4,15 +4,9 @@ broker = utils.Broker(host='127.0.0.1')
 
 exchange_name = 'new_user'
 
-broker.create_exchange(
-    name=exchange_name,
-    type='direct'
-)
+broker.create_exchange(name=exchange_name, type='direct')
 
-broker.send_task(
-    exchange=exchange_name,
-    route='create'
-)
+broker.send_task(exchange=exchange_name, route='create')
 
 print('Нужно завести нового пользователя')
 
